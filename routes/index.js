0ae66cfdd84db53fb5cb529c0444cf87ed08930d
alteon2020 <alteonalves@gmail.com>
2022-03-22 16:39:38 -0300

    Cadastrando dados do formulário de reserva no banco de dados

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var conn = require('./../inc/db');
 var menus = require('./../inc/menus');
+var reservations = require('./../inc/reservations');
 var router = express.Router();
 
 /* GET home page. */
@@ -8,7 +9,7 @@ router.get('/', function (req, res, next) {
   menus.getMenus().then(results => {
     res.render('index', {
       title: 'Restaurante Saboroso!',
-      menus: results, 
+      menus: results,
       isHome: true
     });
   });
@@ -34,11 +35,30 @@ router.get('/menus', function (req, res, next) {
 });
 
 router.get('/reservations', function (req, res, next) {
-  res.render('reservations', {
-    title: 'Reservas - Restaurante Saboroso!',
-    background: 'images/img_bg_2.jpg',
-    h1: "Reserve uma Mesa!"
-  });
+  reservations.render(req, res);
+});
+
+router.post('/reservations', function (req, res, next) {
+
+  if (!req.body.name) {
+    reservations.render(req, res, 'Digite o nome!');
+  } else if (!req.body.email) {
+    reservations.render(req, res, 'Digite o email!');
+  } else if (!req.body.people) {
+    reservations.render(req, res, 'Digite o número de pessoas!');
+  } else if (!req.body.date) {
+    reservations.render(req, res, 'Selecione a data!');
+  } else if (!req.body.time) {
+    reservations.render(req, res, 'Selecione a hora da reserva!');
+  } else {
+    reservations.save(req.body).then(results => {
+      req.body = {};
+      reservations.render(req, res, null, "Reservado com sucesso!");
+    }).catch(err => {
+      reservations.render(req, res, err.message);
+    })
+  }
+
 });
 
 router.get('/services', function (req, res, next) {

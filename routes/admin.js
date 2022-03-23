f771bd48300edd274ffc819033b4793351d0036f
alteon2020 <alteonalves@gmail.com>
2022-03-23 10:27:11 -0300

    Criando a tela de login

--- routes/admin.js
+++ routes/admin.js
@@ -1,33 +1,55 @@
 const express = require("express");
 const router = express.Router();
+const users = require("./../inc/users");
 
-router.get("/", function(req, res, next) {
+router.get("/", function (req, res, next) {
     res.render("admin/index");
 });
 
-router.get("/login", function(req, res, next) {
-    res.render("admin/login");
+router.get("/login", function (req, res, next) {
+    users.render(req, res, null);
 });
 
-router.get("/contacts", function(req, res, next) {
+router.post("/login", function (req, res, next) {
+    if (!req.body.email) {
+        
+        users.render(req, res, "Preencha o campo email!");
+    
+    } else if (!req.body.password) {
+    
+        users.render(req, res, "Preencha o campo senha!");
+    
+    } else {
+        users.login(req.body.email, req.body.password).then(user => {
+            
+            req.session.user = user;
+            res.redirect('/admin');
+
+        }).catch(err => {
+            users.render(req, res, err.message || err);
+        });
+    }
+});
+
+router.get("/contacts", function (req, res, next) {
     res.render("admin/contacts");
 });
 
-router.get("/emails", function(req, res, next) {
+router.get("/emails", function (req, res, next) {
     res.render("admin/emails");
 });
 
-router.get("/menus", function(req, res, next) {
+router.get("/menus", function (req, res, next) {
     res.render("admin/menus");
 });
 
-router.get("/reservations", function(req, res, next) {
+router.get("/reservations", function (req, res, next) {
     res.render("admin/reservations", {
         date: new Date()
     });
 });
 
-router.get("/users", function(req, res, next) {
+router.get("/users", function (req, res, next) {
     res.render("admin/users", {
 
     });

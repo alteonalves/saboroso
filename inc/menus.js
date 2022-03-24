5ef415a91191b6b195102122e14ab98597066cac
alteon2020 <alteonalves@gmail.com>
2022-03-24 15:05:07 -0300

    Configurando o delete

--- inc/menus.js
+++ inc/menus.js
@@ -63,5 +63,20 @@ module.exports = {
                 }
             });
         });
+    },
+    delete(id) {
+        return new Promise((resolve, reject) => {
+            conn.query(`
+            DELETE FROM tb_menus WHERE ID = ?
+            `, [
+                id
+            ], (err, results) => {
+                if (err) {
+                    reject(err);
+                } else {
+                    resolve(results);
+                }
+            })
+        })
     }
 }

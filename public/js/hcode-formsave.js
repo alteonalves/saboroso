bb4fefd45314233187a4bdaaaffadf07227f88f5
alteon2020 <alteonalves@gmail.com>
2022-03-25 10:19:58 -0300

    Configurando evento de alteração de senhas

--- public/js/hcode-formsave.js
+++ public/js/hcode-formsave.js
@@ -1,27 +1,28 @@
-HTMLFormElement.prototype.save = function () {
+HTMLFormElement.prototype.save = function (config) {
 
     const form = this;
 
-    return new Promise((resolve, reject) => {
-        form.addEventListener('submit', e => {
+    form.addEventListener('submit', e => {
 
-            e.preventDefault();
+        e.preventDefault();
 
-            const formData = new FormData(form);
+        const formData = new FormData(form);
 
-            fetch(form.action, {
-                method: form.method,
-                body: formData
-            })
-                .then(response => response.json())
-                .then(json => {
-                    resolve(json);
-                }).catch(err=>{
-                    reject(err);
-                });
+        fetch(form.action, {
+            method: form.method,
+            body: formData
+        })
+            .then(response => response.json())
+            .then(json => {
 
-        });
+                if (json.error) {
+                    if (typeof config.failure === 'function') config.failure(json.error);
+                } else {
+                    if (typeof config.success === 'function') config.success(json);
+                }
+            }).catch(err => {
+                if (typeof config.failure === 'function') config.failure(err);
+            });
+
     });
-
-
 }
